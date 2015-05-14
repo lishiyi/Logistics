7f7fe915d7ce1170fd8d64ad1321d78c371e58b8
lishiyi <lishiyiiii@gmail.com>
2015-05-13 22:54:11 -0400

    v0.00 Item

---
+++ java/src/edu/nyu/lishiyi/Item.java
@@ -0,0 +1,46 @@
+package edu.nyu.lishiyi;
+
+/**
+ * Item
+ * @author lishiyi
+ * @version 0.00
+ */
+
+public class Item {
+
+	private String id;
+	
+	private String name;
+	
+	public Item() {
+		
+	}
+	
+	public Item(String id, String name){
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return getName();
+	}
+
+}
